f407c3399a1d39673ecb1de3e96ceaebda52011e
elvisnjintze <enjintze@gmail.com>
2024-09-11 21:04:37 +0100

    first commit

---
+++ snapface/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FaceSnapComponent } from './face-snap/face-snap.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [FaceSnapComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = '';
+}
