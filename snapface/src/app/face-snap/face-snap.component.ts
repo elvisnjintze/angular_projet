8766005bd0c7a60ac2bf1c814d05be7699b1fb72
elvisnjintze <enjintze@gmail.com>
2024-09-11 21:58:06 +0100

    nous avons appris à ajouter les données dynamique à traver des variables title, description, snaps, et nous avons initialisé notre component avec le méthode ngOnInit de la classe OnInit implementé par notre component

--- snapface/src/app/face-snap/face-snap.component.ts
+++ snapface/src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
@@ -7,6 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit{
+  title!: String;
+  description!:string;
+  createdate!:Date;
+  snaps!:number;
+  imageUrl!:string;
+
+  ngOnInit(): void {
+    this.title = "Bonjour ici elco codeur";
+    this.description = "premier composante que nous avons crée";
+    this.createdate = new Date;
+    this.snaps = 5;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+  }
+
 
 }
