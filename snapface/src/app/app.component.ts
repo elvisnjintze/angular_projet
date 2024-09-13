419398535bac9f8abd1545cfcb705be0a4fbab35
elvisnjintze <enjintze@gmail.com>
2024-09-13 16:33:37 +0100

    La directive  [ngStyle]  permet d'appliquer des styles qui dépendent de valeurs dynamiques, comme un texte qui, plus il y a de snaps, devient de plus en plus vert.
    La directive  [ngStyle]  prend comme argument un objet où les clés correspondent aux styles CSS à modifier, et les valeurs aux valeurs que doivent prendre ces styles.

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit{
       'Mon meilleur ami depuis tout petit !',
       'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
       new Date(),
-      0),
+      45),
       new FaceSnap( 'Elvis william njintze',
         'Moi meme je dérange !',
         'assets/images/20240604_175353.jpg',
@@ -28,7 +28,7 @@ export class AppComponent implements OnInit{
           'Moi meme je dérange !',
           'assets/images/20240604_194419.jpg',
           new Date(),
-          0)];
+          200)];
     
     
         
