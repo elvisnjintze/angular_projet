6cae94a080715d0f60cfc6340355546a89033240
elvisnjintze <enjintze@gmail.com>
2024-09-13 14:59:02 +0100

    ici Le control flow block  @for  vous permet d'insérer un élément dans le DOM pour chaque élément dans un tableau.
    La syntaxeelement of array  vous donne accès à l'élément de chaque itération, vous permettant de l'injecter dans un component, par exemple.
    
    Le paramètre  track  demande une propriété qui sera unique pour chaque élément du tableau (qui sera souvent mais pas toujours son  id  , s'il en a un)

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -11,25 +11,27 @@ import { FaceSnap } from './face-snap/models/face-snap';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit{
-  mySnap!: FaceSnap;
-  otherSnap!: FaceSnap;
-  lastSnap!: FaceSnap;
+  facesnap!: FaceSnap[];
+ 
   ngOnInit(): void {
-    this.mySnap = new FaceSnap( 'Fred kamga William',
+    this.facesnap = [new FaceSnap( 'Fred kamga William',
       'Mon meilleur ami depuis tout petit !',
       'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
       new Date(),
-      0);
-      this.otherSnap = new FaceSnap( 'Elvis william njintze',
+      0),
+      new FaceSnap( 'Elvis william njintze',
         'Moi meme je dérange !',
         'assets/images/20240604_175353.jpg',
         new Date(),
-        0);
-        this.lastSnap = new FaceSnap( 'Elco Business & Technologies',
+        0),
+        new FaceSnap( 'Elco Business & Technologies',
           'Moi meme je dérange !',
           'assets/images/20240604_194419.jpg',
           new Date(),
-          0);
-        this.lastSnap.setlocation('mon bureau à bonamoussadi')
+          0)];
+    
+    
+        
+        this.facesnap[1].setlocation('mon bureau à bonamoussadi')
   }
 }
