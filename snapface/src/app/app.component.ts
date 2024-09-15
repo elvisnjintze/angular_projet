fa4435d96aabdca62a1d5159c2e765d422eecad7
elvisnjintze <enjintze@gmail.com>
2024-09-15 20:20:34 +0100

    Un fichier de routing contient un tableau de type  Routes  qui contient les routes de l'application.
    Une route est un objet de type  { path: 'myPath', component: MyComponent }  qui spécifie le component à afficher pour chaque route.
    
    On appelle  provideRouter  en passant le tableau de routes pour enregistrer les routes dans le routeur Angular.
    
    On ajoute une balise  <router-outlet>  pour dire à quel niveau du template le component de la route active doit être inséré, en ajoutant RouterOutlet aux imports de ce component.
    
    Pour ajouter des fichiers statiques à une application (comme des images), on les stocke dans le dossier  assets

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { HeaderComponent } from './header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FaceSnapListComponent,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
